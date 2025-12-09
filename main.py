96a065f8f2f57a8be86b1195b4b43e69fe36ddac
martinmbungu <martinmbungunjoki@gmail.com>
2025-12-09 09:43:45 -0800

    Update on posting data from the client

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from database import get_products, get_sales
+from flask import Flask, render_template, request, redirect, url_for
+from database import get_products, get_sales, insert_products
 
 
 #flask instance
@@ -16,10 +16,20 @@ def fetch_products():
     products = get_products()
     return render_template("products.html", products=products)
 
+
 @app.route("/sales")
 def fetch_sales():
     sales = get_sales()
     return render_template("sales.html", sales=sales)
+
+@app.route("/add_products", methods=['GET', 'POST'])
+def add_products():
+    product_name = request.form["product_name"]
+    buying_price = request.form["buying_price"]
+    selling_price = request.form["selling_price"]
+    new_product = (product_name, buying_price, selling_price)
+    insert_products(new_product)
+    return redirect(url_for('fetch_products'))
 
 @app.route("/dashboard")
 def dashboard():
